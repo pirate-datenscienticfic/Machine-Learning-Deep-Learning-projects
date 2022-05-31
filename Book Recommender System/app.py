3364718a7c274494b1e51dbc4a3b503bddc67bb4
pirate-datenscienticfic <shubhamyadav7122000@gmail.com>
2022-05-31 10:41:34 +0530

    Book Recommendation system and Spam Detection

--- Book Recommender System/app.py
+++ Book Recommender System/app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import pickle
+
+popular_df = pickle.load(open('popular.pkl'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           book_name = list(popular_df['Book-Title'].values),
+                           author = list(popular_df['Book-Author'].values),
+                           image = list(popular_df['Image-URL-M'].values),
+                           votes = list(popular_df['Num-Ratings'].values),
+                           rating = list(popular_df['Avg-Ratings'].values)
+                        )
 
 if __name__ == '__main__':
     app.run(debug=True)
